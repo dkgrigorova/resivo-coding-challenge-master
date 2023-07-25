3e44c3ca71552e1156299fa953a5aabcfd9b84ec
Denitsa Grigorova <denitsa.grigorova@accedia.com>
2023-07-25 12:53:09 +0300

    feat: implement additional DoorMapper tests

--- src/server/mappers/DoorMapper.test.ts
+++ src/server/mappers/DoorMapper.test.ts
@@ -2,6 +2,7 @@ import { BuildingDto } from '@/__mocks__/dtos/BuidlingDto';
 import { DoorDto } from '@/__mocks__/dtos/DoorDto';
 import { Door } from '@/models/Door';
 import { DoorMapper } from './DoorMapper';
+import { ApartmentDto } from '@/__mocks__/dtos/ApartmentDto';
 
 const buildingDto: BuildingDto = {
   id: '63f4e0797e85310fee059022',
@@ -11,6 +12,13 @@ const buildingDto: BuildingDto = {
   city: 'Zurich',
 };
 
+const apartmentDto: ApartmentDto = {
+  id: '63f4e2825abc011556da74af',
+  name: 'Apartment 1.1',
+  floor: 1,
+  building_id: buildingDto.id,
+};
+
 const doorDto: DoorDto = {
   id: '63f4d82ef04826419cc6eaeb',
   name: 'Building Main Entrance',
@@ -18,6 +26,7 @@ const doorDto: DoorDto = {
   connection_status: 'online',
   last_connection_status_update: '2023-02-22T02:38:40.374Z',
   building_id: buildingDto.id,
+  apartment_id: '63f4e2825abc011556da74af',
 };
 
 describe('DoorMapper', () => {
@@ -28,6 +37,48 @@ describe('DoorMapper', () => {
   });
 
   it('should map dto to Door model', () => {
+    const door = doorMapper.toDomain(
+      doorDto,
+      {
+        [buildingDto.id]: buildingDto,
+      },
+      {
+        [apartmentDto?.id]: apartmentDto,
+      },
+    );
+
+    expect(door).toMatchObject<Door>({
+      id: doorDto.id,
+      name: doorDto.name,
+      buildingName: `${buildingDto.street} ${buildingDto.street_no}`,
+      connectionType: doorDto.connection_type,
+      connectionStatus: doorDto.connection_status,
+      lastConnectionStatusUpdate: doorDto.last_connection_status_update,
+      apartmentName: apartmentDto.name,
+    });
+  });
+
+  it('should set building name to "n/a" if no matching building is found', () => {
+    const door = doorMapper.toDomain(
+      doorDto,
+      {},
+      {
+        [apartmentDto?.id]: apartmentDto,
+      },
+    );
+
+    expect(door).toMatchObject<Door>({
+      id: doorDto.id,
+      name: doorDto.name,
+      buildingName: 'n/a',
+      connectionType: doorDto.connection_type,
+      connectionStatus: doorDto.connection_status,
+      lastConnectionStatusUpdate: doorDto.last_connection_status_update,
+      apartmentName: apartmentDto.name,
+    });
+  });
+
+  it('should set apartment name to "n/a" if no matching apartment is found', () => {
     const door = doorMapper.toDomain(doorDto, {
       [buildingDto.id]: buildingDto,
     });
@@ -39,19 +90,7 @@ describe('DoorMapper', () => {
       connectionType: doorDto.connection_type,
       connectionStatus: doorDto.connection_status,
       lastConnectionStatusUpdate: doorDto.last_connection_status_update,
-    });
-  });
-
-  it('should set building name to "n/a" if no matching building is found', () => {
-    const door = doorMapper.toDomain(doorDto, {});
-
-    expect(door).toMatchObject<Door>({
-      id: doorDto.id,
-      name: doorDto.name,
-      buildingName: 'n/a',
-      connectionType: doorDto.connection_type,
-      connectionStatus: doorDto.connection_status,
-      lastConnectionStatusUpdate: doorDto.last_connection_status_update,
+      apartmentName: 'n/a',
     });
   });
 });
