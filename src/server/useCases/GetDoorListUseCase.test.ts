d052a37d5b5866603099bf518c5328a4b4a8cc4d
Denitsa Grigorova <denitsa.grigorova@accedia.com>
2023-07-25 12:54:52 +0300

    feat: implement additional GetDoorListUseCase tests

--- src/server/useCases/GetDoorListUseCase.test.ts
+++ src/server/useCases/GetDoorListUseCase.test.ts
@@ -3,6 +3,7 @@ import { HttpError } from 'http-errors';
 import { DoorRepository } from '@/server/repositories/DoorRepository';
 import { BuildingRepository } from '@/server/repositories/BuildingRepository';
 import { GetDoorListUseCase } from './GetDoorListUseCase';
+import { ApartmentRepository } from '../repositories/ApartmentRepository';
 
 describe('GetDoorListUseCase', () => {
   let getDoorListUseCase: GetDoorListUseCase;
@@ -21,10 +22,15 @@ describe('GetDoorListUseCase', () => {
       .spyOn(BuildingRepository.prototype, 'getAllBuildings')
       .mockImplementation(() => Promise.resolve([]));
 
+    const getAllApartmentsSpy = jest
+      .spyOn(ApartmentRepository.prototype, 'getAllApartments')
+      .mockImplementation(() => Promise.resolve([]));
+
     await getDoorListUseCase.execute();
 
     expect(getAllDoorsSpy).toHaveBeenCalledTimes(1);
     expect(getAllBuildingsSpy).toHaveBeenCalledTimes(1);
+    expect(getAllApartmentsSpy).toHaveBeenCalledTimes(1);
   });
 
   it('should throw if getAllBuildings request fails', async () => {
@@ -38,6 +44,10 @@ describe('GetDoorListUseCase', () => {
         throw new Error();
       });
 
+    const getAllApartmentsSpy = jest
+      .spyOn(ApartmentRepository.prototype, 'getAllApartments')
+      .mockImplementation(() => Promise.resolve([]));
+
     try {
       await getDoorListUseCase.execute();
     } catch (error) {
@@ -46,7 +56,36 @@ describe('GetDoorListUseCase', () => {
 
     expect(getAllDoorsSpy).toHaveBeenCalledTimes(1);
     expect(getAllBuildingsSpy).toHaveBeenCalledTimes(1);
+    expect(getAllApartmentsSpy).not.toHaveBeenCalled();
 
-    expect.assertions(3);
+    expect.assertions(4);
+  });
+
+  it('should throw if getAllApartments request fails', async () => {
+    const getAllDoorsSpy = jest
+      .spyOn(DoorRepository.prototype, 'getAllDoors')
+      .mockImplementation(() => Promise.resolve([]));
+
+    const getAllBuildingsSpy = jest
+      .spyOn(BuildingRepository.prototype, 'getAllBuildings')
+      .mockImplementation(() => Promise.resolve([]));
+
+    const getAllApartmentsSpy = jest
+      .spyOn(ApartmentRepository.prototype, 'getAllApartments')
+      .mockImplementation(() => {
+        throw new Error();
+      });
+
+    try {
+      await getDoorListUseCase.execute();
+    } catch (error) {
+      expect(error).toBeInstanceOf(HttpError);
+    }
+
+    expect(getAllDoorsSpy).toHaveBeenCalledTimes(1);
+    expect(getAllBuildingsSpy).toHaveBeenCalledTimes(1);
+    expect(getAllApartmentsSpy).toHaveBeenCalledTimes(1);
+
+    expect.assertions(4);
   });
 });
